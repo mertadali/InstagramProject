e88d405814b6f1d1734ad16d9ab2c1df06c97907
Mert Adalı <mertadali605@gmail.com>
2024-02-27 02:19:26 +0300

    xml bugs fixed!

--- app/src/main/java/com/mertadali/instagramcloneproject/MainActivity.kt
+++ app/src/main/java/com/mertadali/instagramcloneproject/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.mertadali.instagramcloneproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.mertadali.instagramcloneproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        auth = FirebaseAuth.getInstance()
 
 
 
@@ -20,11 +25,35 @@ class MainActivity : AppCompatActivity() {
 
     private  fun login(view: View){
 
-    }
-
-    private fun signup(view: View){
 
     }
+
+    fun signUp(view: View){
+
+        val email = binding.username.text.toString()
+        val password = binding.password.text.toString()
+
+       if (email == "" || password == ""){
+           Toast.makeText(this,"Yor email or password empty",Toast.LENGTH_LONG).show()
+       }else{
+           auth.createUserWithEmailAndPassword(email,password)
+       // Firebase sunucularına istedik atılıyor bir mesaj alındığında intent yapılmalı yani asyc işlem yapmalıyız.
+               .addOnSuccessListener {
+                   val intent = Intent(this@MainActivity,FeedActivity::class.java)
+                   startActivity(intent)
+                   finish()
+
+               }.addOnFailureListener {
+                   Toast.makeText(this@MainActivity,it.localizedMessage,Toast.LENGTH_LONG).show()
+
+               }
+
+       }
+
+
+    }
+
+
 
 
 }
